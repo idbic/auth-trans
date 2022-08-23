728bf408b7408112f16a454a3330961158984437
idbic <dbicga@gmail.com>
2022-08-23 13:58:40 -0500

    added carousel and about comp stubbed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import NavBar from './Components /NavBar';
+import Slideshow from './Components /Slideshow';
+import About from './Components /About'
 
 function App() {
   return (
     <>
     <NavBar/>
+    <Slideshow/>
+    <About/>
     </>
   );
 }
---
+++ src/Components /Slideshow.jsx
@@ -0,0 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Mechanic from '../Media/mechanic.jpg'
+import OldTruck from '../Media/oldtrucks.jpg'
+import Trans from '../Media/transmission.jpg'
+
+function Slideshow() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Mechanic}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={OldTruck}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Trans}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slideshow;
